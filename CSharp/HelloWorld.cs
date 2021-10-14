ddddc11b5e9e35efa7ed17cfda4f9be3046b2acf
hey-mikey <michaelarmes1@gmail.com>
2021-10-14 11:08:18 -0700

    hello world in c# console

---
+++ CSharp/HelloWorld.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public static class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World! Today is " + System.DateTime.Now.ToString(System.Globalization.CultureInfo.CurrentCulture));
+        Console.Read();
+    }
+}
